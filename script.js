85a0d7624a8470a0dc985142b195df7b24684765
Preston Hales <PrestonGHales@gmail.comgit config --global user.email PrestonGHales@gmail.comgit config --global user.email PrestonGHales@gmail.com>
2014-01-25 13:49:54 -0700

    What I have of my codepen so far

--- script.js
+++ script.js
@@ -31,6 +31,92 @@ Reponse Objects:
 
 $('document').ready(function() {
 
-	//put code here
+	codepen.objects.User = {
+		name: null,
+		email: null,
+		username: null,
+		password: null,
+		is_loged_in: false
+	};
 
-});
+	var NewUser = Object.create(codepen.objects.User, {
+		firstname: {
+			writable: true,
+			enumerable: true,
+			value: ''
+		},
+		lastname: {
+			writable: true,
+			enumberable: true,
+			value: ''
+		}
+	});
+	//hide the signup on load
+	//bring up the signup element if you click signup, and hide the login element
+		$('.signup-form').hide();
+	
+		$('.signup-form-btn').click(function() {
+			$('.signup-form').show();
+			$('.login-form').hide();
+			$(".login-form-btn").removeClass("active");
+			$(".signup-form-btn").addClass("active");
+
+		});
+	 	$('#login-ht').click(function() {
+			$('.login-form').show();
+			$('.signup-form').hide();
+			$(".login-form-btn").addClass("active");
+			$(".signup-form-btn").removeClass("active");
+
+		});
+//.btn-login
+	
+	//grab the text(value) from the username field and password field and store them in variables
+	//instantiate an objet with the username and password
+	// call the login method and pass it our previously instantiated object
+	// and handle the response
+		$('.btn-login').click(function() {
+			var user = Object.create(NewUser);
+			user.username = $("login-username-field").val();
+			user.password = $("login-password-field").val();
+			var response = codepen.api.login(user);
+			if(response.success == true) {
+				$('.login-form .form-feedback').html("You're logged in!");
+			}
+		}); //ending login click
+
+	// $('login-username-field').click(function() {
+	// 	$('login-password-field').removeClass('backgnd');
+	// 	$('login-username-field').addClass('backgnd');
+
+	// });
+
+	// $('login-password-field').click(function() {
+	// 	$('login-password-field').addClass('backgnd');
+	// 	$('login-username-field').removeClass('backgnd');
+	// });
+
+	
+	$(".btn-signup").click(function() {
+		var signupUser = {};
+		signupUser.name = $("#signup-name-field").val();
+		signupUser.email = $("#signup-email-field").val();
+		signupUser.username = $("#signup-username-field").val();
+		signupUser.password = $("#signup-password-field").val();
+		console.log((codepen.api.signup(signupUser)));
+
+
+
+
+
+	});
+
+
+
+
+
+
+
+
+
+}); //ending ready
